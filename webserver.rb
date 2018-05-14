e73f2cdc6cf6649978700d9bf39f69e06665fb37
Yinuo Chen <yinuochen@Yinuos-MacBook-Pro.local>
2018-05-13 23:36:43 -0700

    edited ruby script

--- webserver.rb
+++ webserver.rb
@@ -4,8 +4,20 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-file '/var/www/html/index.php' do
-  path 'index.html'
+file '/var/www/html/index.html' do
+  content '
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Hello Onica</title>
+<meta http-equiv=\"Content-Type\" content=\"text/html;charset=ISO-8859-1\">
+</head>
+<body>
+<h1>Hello World <script>document.write(document.location.hostname)</script></h1>
+</body>
+</html>
+'
   mode '0755'
   owner 'apache'
   group 'apache'
